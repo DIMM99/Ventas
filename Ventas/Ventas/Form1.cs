b776948c9be523284d785729f487cd340e586783
Daniel Medina <Daniel Medina@DESKTOP-9NVVCBU>
2021-09-22 00:18:06 -0600

    creando programa hola mundo

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundoo");
+            MessageBox.Show("Hola Mundo");
         }
     }
 }
